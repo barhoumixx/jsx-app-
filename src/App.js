0862af6cbdc2092534348ff1bcc6e807cf32253c
wassim barhoumi <wassimix14@gmail.com>
2021-06-15 21:36:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import "./style.css";
+import myimageinsrc from "./imageInSrc.jpg"
+
+function App() {
+  return(
+    <div className="App">
+      <header className="App-header">
+      <div style={{border:'solid 1px black',maxWidth:'100vw'}}/>
+      <h1 className="title-red">wassim barhoumi</h1>
+      <br/>
+      <img src={myimageinsrc} width="320" height="240" alt= "imageInSrc"/>
+      <br/>
+      <img src="/imageInPublic.jpg" alt = "imageinpublic"/>
+      <video width="320" height="240" controls/>
+      <source src="myVideo.mp4" type="video/mp4"/>
+      </header>
+   </div>
+ );
+}
+    
+
+export default App;
